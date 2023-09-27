6998c83cd8d01850652ee35b91c2ddba74eff1e0
RT1648 <rohitttiwari52@gmail.com>
2023-09-27 11:25:41 +0530

    Builder: Import `ccache` flags in environment

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,12 @@ lunch $MAKEFILENAME-$VARIENT
 export SKIP_ABI_CHECKS=true
 export SKIP_API_CHECKS=true
 export ALLOW_MISSING_DEPENDENCIES=true
+export CCACHE_DIR=/tmp/ccache
+export CCACHE_EXEC=$(which ccache)
+export USE_CCACHE=1
+ccache -M 50G
+ccache -o compression=true
+ccache -z
 $EXTRACMD
 $TARGET -j$(nproc --all) & sleep 95m || curl --upload-file ./out/error.log https://free.keep.sh > link.txt && cat link.txt
 }
