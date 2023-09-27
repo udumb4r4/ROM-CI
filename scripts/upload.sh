a6490d479f3792589040efb7528b5b2d3f36ab91
RT1648 <rohitttiwari52@gmail.com>
2023-09-27 14:04:02 +0530

    Builder: Change docker
    Signed-off-by: RT1648 <rohitttiwari52@gmail.com>

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -22,7 +22,7 @@ uccache
 
 rom_upload() {
 cd ~/
-curl --upload-file ./out/target/product/$DEVICE/Sup*.zip https://free.keep.sh > link.txt && cat link.txt
+curl --upload-file ./out/target/product/$DEVICE/Sup*.zip https://free.keep.sh > link0.txt && cat link0.txt
 curl --upload-file ./out/target/product/$DEVICE/boot.img https://free.keep.sh > link1.txt && cat link1.txt
 }
 echo "Uploading Rom & Boot From /Out"
