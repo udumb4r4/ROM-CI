e8be42522e7e5d6135abf27c678e7afe5b29c9c5
Rohit Tiwari <rohitttiwari52@gmail.com>
2023-10-05 10:12:48 +0530

    Builder: Cleanup
    Signed-off-by: Rohit Tiwari <rohitttiwari52@gmail.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,13 +3,10 @@ build() {
 source build/envsetup.sh || . build/envsetup.sh
 bash device/xiaomi/fleur/prebuilts/vendor.sh
 lunch $MAKEFILENAME-$VARIENT
-export SKIP_ABI_CHECKS=true
-export SKIP_API_CHECKS=true
-export ALLOW_MISSING_DEPENDENCIES=true
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
-ccache -M 50G
+ccache -M 100G
 ccache -o compression=true
 ccache -z
 $EXTRACMD
